7f2fa52eaf4cb9d7ad873134897f5833aea67d4e
vulnerabbity <vulnerabbity@example.com>
2022-05-01 10:18:01 +0700

    refactoring

--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from "@angular/core"
+import { NgModule, Provider } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { RouteReuseStrategy } from "@angular/router"
-
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular"
-
 import { RootComponent } from "./root.component"
 import { AppRoutingModule } from "./root-routing.module"
+
+const IonicRouteStrategyProvider: Provider = {
+  provide: RouteReuseStrategy,
+  useClass: IonicRouteStrategy,
+}
 
 @NgModule({
   declarations: [RootComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [RootComponent]
+  providers: [IonicRouteStrategyProvider],
+  bootstrap: [RootComponent],
 })
 export class RootModule {}
