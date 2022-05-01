27dcbda5a7e62512c818bc928bfd06eae838d975
vulnerabbity <vulnerabbity@example.com>
2022-05-01 18:07:02 +0700

    refactoring

--- src/app/root/root.component.spec.ts
+++ src/app/root/root.component.spec.ts
@@ -1,18 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
 import { TestBed, waitForAsync } from "@angular/core/testing"
 
-import { AppComponent } from "./root.component"
+import { RootComponent } from "./root.component"
 
 describe("AppComponent", () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      declarations: [RootComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents()
   }))
 
   it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent)
+    const fixture = TestBed.createComponent(RootComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
   })
