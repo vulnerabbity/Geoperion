02d2121a5bedf5b9e340f94a5b8d992aec47f7af
vulnerabbity <vulnerabbity@example.com>
2022-05-01 20:04:18 +0700

    add basic styles

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core"
+import { AppCommonModule } from "src/app/common/common.module"
 import { HomePage } from "./home.page"
 
 @NgModule({
+  imports: [AppCommonModule],
   declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core"
+import { RouterService } from "src/app/common/services/router.service"
 
 @Component({
   templateUrl: "./home.page.html",
-  styleUrls: ["./home.page.scss"]
+  styleUrls: ["./home.page.scss"],
 })
 export class HomePage {}
