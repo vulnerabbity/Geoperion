a9b54d05fc4fc27bfc270afbe2bec853e3022bad
vulnerabbity <vulnerabbity@example.com>
2022-05-02 14:54:56 +0700

    add ionic storage library

--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -5,6 +5,8 @@ import { IonicModule, IonicRouteStrategy } from "@ionic/angular"
 import { RootComponent } from "./root.component"
 import { AppRoutingModule } from "./root-routing.module"
 import { AppCommonModule } from "../common/common.module"
+import { IonicStorageModule } from "@ionic/storage-angular"
+import { Storage } from "@ionic/storage-angular"
 
 const IonicRouteStrategyProvider: Provider = {
   provide: RouteReuseStrategy,
@@ -14,8 +16,19 @@ const IonicRouteStrategyProvider: Provider = {
 @NgModule({
   declarations: [RootComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AppCommonModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AppCommonModule,
+    IonicStorageModule.forRoot(),
+  ],
   providers: [IonicRouteStrategyProvider],
   bootstrap: [RootComponent],
 })
-export class RootModule {}
+export class RootModule {
+  constructor(private storage: Storage) {
+    // create storage required by ionic storage library
+    this.storage.create()
+  }
+}
