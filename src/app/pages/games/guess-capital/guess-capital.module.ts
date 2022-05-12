725dfd5d0e2bc22f4667aee5b369a96906544d44
vulnerabbity <vulnerabbity@example.com>
2022-05-12 22:40:10 +0700

    implement guess capital game

--- src/app/pages/games/guess-capital/guess-capital.module.ts
+++ src/app/pages/games/guess-capital/guess-capital.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core"
 import { AppCommonModule } from "src/app/common/common.module"
+import { CountriesGamesModule } from "src/app/features/games/countries/countries-games.module"
 import { GuessCapitalGamePage } from "./guess-capital.page"
 
 @NgModule({
-  imports: [AppCommonModule],
+  imports: [AppCommonModule, CountriesGamesModule],
   declarations: [GuessCapitalGamePage],
 })
 export class GuessCapitalGameModule {}
--- src/app/pages/games/guess-capital/guess-capital.page.ts
+++ src/app/pages/games/guess-capital/guess-capital.page.ts
@@ -1,7 +1,98 @@
-import { Component } from "@angular/core"
+import { Component, OnDestroy, OnInit } from "@angular/core"
+import { AnswersComponentPage } from "src/app/common/components/game/game-answers/game-answers.component"
+import { Country } from "src/app/data/countries.data"
+import { CountryPage } from "src/app/features/games/countries/countries-games.interface"
+import { CountriesGamesState } from "src/app/features/games/countries/countries-games.state"
+import { getFlagFullPath } from "src/assets/images/flags/flags-getter"
 
 @Component({
   templateUrl: "./guess-capital.page.html",
-  styleUrls: ["./guess-capital.page.scss"],
+  styleUrls: ["./guess-capital.page.scss", "../games.shared-styles.scss"],
 })
-export class GuessCapitalGamePage {}
+export class GuessCapitalGamePage implements OnDestroy, OnInit {
+  private pages: CountryPage[] = []
+  private currentPageIndex = 0
+  private currentPage: CountryPage | undefined = undefined
+  private rightCountry: Country | undefined = undefined
+
+  private stateSub = this.handleStateChange()
+
+  constructor(private countriesState: CountriesGamesState) {}
+
+  ngOnInit(): void {
+    this.countriesState.startNewState()
+  }
+
+  hasCurrentPage() {
+    return !!this.currentPage
+  }
+
+  getFlag() {
+    const flag = this.rightCountry?.flag ?? ""
+
+    return getFlagFullPath(flag)
+  }
+
+  getRightCountryName() {
+    return this.rightCountry?.name ?? "Country"
+  }
+
+  getTotalPages() {
+    return this.pages.length
+  }
+
+  getAnsweredQuestionsNumber() {
+    let answered = 0
+    for (let page of this.pages) {
+      const isAnswered = !!page.selectedAnswerIndex
+      if (isAnswered) {
+        answered += 1
+      }
+    }
+
+    return answered
+  }
+
+  // Allows to use concrete field to display answers
+  getAnswersPage(): AnswersComponentPage | undefined {
+    const currentPage = this.currentPage
+    if (currentPage === undefined) {
+      return undefined
+    }
+
+    // select capital as displayed answer
+    const answersOptions = currentPage.options.map(option => {
+      return { optionName: option.capital }
+    })
+
+    return { ...currentPage, options: answersOptions }
+  }
+
+  ngOnDestroy(): void {
+    this.stateSub.unsubscribe()
+  }
+
+  private handleStateChange() {
+    return this.countriesState.state$.subscribe(newState => {
+      this.pages = newState.pages
+      this.currentPageIndex = newState.currentPageIndex
+
+      this.updateCurrentPage()
+      this.updateRightCountry()
+    })
+  }
+
+  private updateRightCountry() {
+    const currentPage = this.currentPage
+    if (currentPage === undefined) {
+      return
+    }
+
+    const rightCountry = currentPage.options[currentPage.rightAnswerIndex]
+    this.rightCountry = rightCountry
+  }
+
+  private updateCurrentPage() {
+    this.currentPage = this.pages[this.currentPageIndex]
+  }
+}
