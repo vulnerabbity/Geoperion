29396ec2b3b414b84ef5dd0aa77a375acd9acd34
vulnerabbity <vulnerabbity@example.com>
2022-05-13 11:02:42 +0700

    add guess-population boiler plate

---
+++ src/app/pages/games/guess-population/guess-population.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core"
+import { GuessPopulationGamePage } from "./guess-population.page"
+
+@NgModule({
+  declarations: [GuessPopulationGamePage],
+})
+export class GuessPopulationGameModule {}
---
+++ src/app/pages/games/guess-population/guess-population.page.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core"
+
+@Component({
+  templateUrl: "./guess-population.page.html",
+  styleUrls: ["./guess-population.page.scss"],
+})
+export class GuessPopulationGamePage {}
