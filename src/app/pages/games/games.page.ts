9e686d035b5123c31f04d2289c127079de9b2b14
vulnerabbity <vulnerabbity@example.com>
2022-05-13 15:33:14 +0700

    add guess population game

--- src/app/pages/games/games.page.ts
+++ src/app/pages/games/games.page.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { FlagsService } from "src/app/features/flags/flags.service"
 import { GamesPaths } from "./games-routing.module"
 
 @Component({
@@ -7,6 +8,17 @@ import { GamesPaths } from "./games-routing.module"
 })
 export class GamesPage {
   paths = GamesPaths
+  flags = {
+    guessCountry: this.getRandomFlag(),
+    guessCapital: this.getRandomFlag(),
+    guessPopulation: this.getRandomFlag(),
+  }
 
-  constructor() {}
+  constructor(private flagsService: FlagsService) {}
+
+  private getRandomFlag() {
+    const flag = this.flagsService.getRandomFlag()
+
+    return flag
+  }
 }
