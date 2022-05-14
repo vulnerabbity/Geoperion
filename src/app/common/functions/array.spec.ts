9ed0039637aa104dbc17c67587d70dd4e5862a39
vulnerabbity <vulnerabbity@example.com>
2022-05-14 17:37:12 +0700

    add array sort fuctions

--- src/app/common/functions/array.spec.ts
+++ src/app/common/functions/array.spec.ts
@@ -1,4 +1,4 @@
-import { swapArrayItems } from "./array.functions"
+import { sortAscendingFunction, sortDescendingFunction, swapArrayItems } from "./array.functions"
 
 describe("Array functions", () => {
   describe("swapArrayItems", () => {
@@ -7,6 +7,42 @@ describe("Array functions", () => {
 
       swapArrayItems(sample, 0, 2)
       expect(sample).toEqual(["three", "two", "one"])
+    })
+  })
+
+  describe("sortAscendingFunction", () => {
+    it("should determine ascending order for numbers", () => {
+      const dataset = [14, 7, 6, 9, 0]
+      const expectedSortedDataset = [0, 6, 7, 9, 14]
+
+      dataset.sort(sortAscendingFunction)
+      expect(dataset).toEqual(expectedSortedDataset)
+    })
+
+    it("should determine ascending order for strings", () => {
+      const dataset = ["b", "D", "a", "c"]
+      const expectedSortedDataset = ["a", "b", "c", "D"]
+
+      dataset.sort(sortAscendingFunction)
+      expect(dataset).toEqual(expectedSortedDataset)
+    })
+  })
+
+  describe("sortDescendingFunction", () => {
+    it("should determine descending order for numbers", () => {
+      const dataset = [1, 6, 2, 8, 4, 0]
+      const expectedSortedDataset = [8, 6, 4, 2, 1, 0]
+
+      dataset.sort(sortDescendingFunction)
+      expect(dataset).toEqual(expectedSortedDataset)
+    })
+
+    it("should determine descending order for strings", () => {
+      const dataset = ["b", "C", "a", "d"]
+      const expectedSortedDataset = ["d", "C", "b", "a"]
+
+      dataset.sort(sortDescendingFunction)
+      expect(dataset).toEqual(expectedSortedDataset)
     })
   })
 })
