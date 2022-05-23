fc7a4da40bc0b69171faf2adaa92ac462c473890
vulnerabbity <vulnerabbity@example.com>
2022-05-23 23:29:24 +0700

    translation: settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core"
+import { Component, OnDestroy, OnInit } from "@angular/core"
+import { LanguageService } from "src/app/common/language/language.service"
+import { Translation } from "src/app/common/language/translations/en.translation"
 
 @Component({
   templateUrl: "./settings.page.html",
   styleUrls: ["./settings.page.scss"],
 })
-export class SettingsPage {}
+export class SettingsPage {
+  private translation = this.languageService.translation
+
+  constructor(private languageService: LanguageService) {}
+
+  getTitle() {
+    return this.translation.settings.settings
+  }
+}
