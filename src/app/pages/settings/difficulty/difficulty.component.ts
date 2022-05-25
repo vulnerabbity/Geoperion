ba934634cd2915dd8ea58b59f32ad5b707d70aa1
vulnerabbity <vulnerabbity@example.com>
2022-05-25 21:48:51 +0700

    refactoring: (difficulty component) removed dependencies

--- src/app/pages/settings/difficulty/difficulty.component.ts
+++ src/app/pages/settings/difficulty/difficulty.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from "@angular/core"
-import { LanguageService } from "src/app/common/language/language.service"
+import { Component, Input } from "@angular/core"
+import { LanguageService, LanguageServiceInstance } from "src/app/common/language/language.service"
 import { GameConfig } from "src/app/features/settings/game-config"
 import { GameConfigState } from "src/app/features/settings/game-config.state"
 import { GameDifficulty } from "src/app/features/settings/settings.interface"
@@ -9,42 +9,19 @@ import { GameDifficulty } from "src/app/features/settings/settings.interface"
   templateUrl: "./difficulty.component.html",
   styleUrls: ["./difficulty.component.scss"],
 })
-export class SettingsPageDifficultyComponent implements OnDestroy {
+export class SettingsPageDifficultyComponent {
+  @Input("difficulty")
   currentDifficulty: GameDifficulty = "easy"
 
   difficulties: GameDifficulty[] = ["easy", "medium", "hard"]
 
-  private stateSub = this.subscribeToState()
-
-  constructor(
-    private config: GameConfig,
-    private configState: GameConfigState,
-    private languageService: LanguageService,
-  ) {}
+  private translation = LanguageServiceInstance.translation
 
   getVisualDifficulty(difficulty: GameDifficulty) {
-    return this.getTranslation().settings.difficulty[difficulty]
+    return this.translation.settings.difficulty[difficulty]
   }
 
   getTitle() {
-    return this.getTranslation().settings.difficulty.difficulty
-  }
-
-  ngOnDestroy(): void {
-    this.stateSub.unsubscribe()
-  }
-
-  async set() {
-    await this.config.difficulty.set(this.currentDifficulty)
-  }
-
-  private getTranslation() {
-    return this.languageService.translation
-  }
-
-  private subscribeToState() {
-    return this.configState.config$.subscribe(newConfig => {
-      this.currentDifficulty = newConfig.difficulty
-    })
+    return this.translation.settings.difficulty.difficulty
   }
 }
