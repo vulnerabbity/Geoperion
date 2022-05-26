4087d277c78b47622324e712d5ec50ebd07eedcc
vulnerabbity <vulnerabbity@example.com>
2022-05-26 15:22:39 +0700

    refactoring: (settings page) (game lenght component)
    Replaced complex typescript logic by events

--- src/app/pages/settings/game-length/game-length.component.ts
+++ src/app/pages/settings/game-length/game-length.component.ts
@@ -1,31 +1,19 @@
-import { Component, OnDestroy } from "@angular/core"
-import { GameConfig } from "src/app/features/settings/game-config"
-import { GameConfigState } from "src/app/features/settings/game-config.state"
+import { Component, Input } from "@angular/core"
 import { GameLength } from "src/app/features/settings/settings.interface"
+import { SettingsPageEvents } from "../settings.events"
 
 @Component({
   selector: "settings__game-length",
   templateUrl: "./game-length.component.html",
   styleUrls: ["./game-length.component.scss"],
 })
-export class SettingsPageGameLengthComponent implements OnDestroy {
+export class SettingsPageGameLengthComponent {
+  @Input("gameLength")
   currentLength: GameLength = "short"
 
-  private configSub = this.subscribeToConfig()
+  constructor(private events: SettingsPageEvents) {}
 
-  constructor(private configState: GameConfigState, private config: GameConfig) {}
-
-  ngOnDestroy(): void {
-    this.configSub.unsubscribe()
-  }
-
-  set() {
-    this.config.length.set(this.currentLength)
-  }
-
-  private subscribeToConfig() {
-    return this.configState.config$.subscribe(config => {
-      this.currentLength = config.length
-    })
+  changeGameLength() {
+    this.events.general.gameLengthChanged$.next(this.currentLength)
   }
 }
