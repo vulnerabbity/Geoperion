dcf0c385be41c5052ba31cab85603bf9aff3d8bd
vulnerabbity <vulnerabbity@example.com>
2022-05-26 15:00:09 +0700

    refactoring: (settings page) (accent component)
    Replaced complex typescript logic by events to handle in single place

--- src/app/pages/settings/theme/accent/accent.component.ts
+++ src/app/pages/settings/theme/accent/accent.component.ts
@@ -1,53 +1,19 @@
-import { Component, OnDestroy } from "@angular/core"
-import { Subject, timer } from "rxjs"
-import { debounce } from "rxjs/operators"
-import { AppThemeEventsBus } from "src/app/features/settings/theme/theme.events-bus"
-import { AppThemeState } from "src/app/features/settings/theme/theme.state"
+import { Component, Input } from "@angular/core"
+import { getDefaultThemeHexAccent } from "src/app/features/settings/theme/theme-accent.config"
+import { SettingsPageEvents } from "../../settings.events"
 
 @Component({
   selector: "settings__accent",
   templateUrl: "./accent.component.html",
   styleUrls: ["./accent.component.scss"],
 })
-export class SettingsPageAccentComponent implements OnDestroy {
-  accent: string = this.getDefaultBackground()
+export class SettingsPageAccentComponent {
+  @Input("hexAccent")
+  hexAccent: string = getDefaultThemeHexAccent()
 
-  private accentChanged$ = new Subject<void>()
+  constructor(private events: SettingsPageEvents) {}
 
-  private onAccentChangeSub = this.saveAccentOnChange()
-  private themeStateSub = this.subscribeToTheme()
-
-  constructor(private themeState: AppThemeState, private themeEvents: AppThemeEventsBus) {}
-
-  ngOnDestroy(): void {
-    this.onAccentChangeSub.unsubscribe()
-    this.themeStateSub.unsubscribe()
-  }
-
-  emitAccentChanged() {
-    this.accentChanged$.next()
-  }
-
-  private saveAccent() {
-    this.themeEvents.changeAccent$.next({ hexColor: this.accent })
-  }
-
-  private getDefaultBackground() {
-    return this.themeState.getDefault().hexAccent
-  }
-
-  private subscribeToTheme() {
-    return this.themeState.theme$.subscribe(theme => {
-      this.accent = theme.hexAccent
-    })
-  }
-
-  private saveAccentOnChange() {
-    // if many events: save every 100ms
-    const debouncedAccentChange$ = this.accentChanged$.pipe(debounce(() => timer(100)))
-
-    return debouncedAccentChange$.subscribe(() => {
-      this.saveAccent()
-    })
+  changeAccent() {
+    this.events.accentChanged$.next({ hexColor: this.hexAccent })
   }
 }
