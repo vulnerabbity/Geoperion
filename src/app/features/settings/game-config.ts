09cc3782792bf9d6fc7402fa4c482727f81ba20d
vulnerabbity <vulnerabbity@example.com>
2022-05-29 22:12:48 +0700

    refactoring: (renaming)
    Renamed GameLenghtConfig => GameLenghtStorage

--- src/app/features/settings/game-config.ts
+++ src/app/features/settings/game-config.ts
@@ -1,9 +1,6 @@
 import { Injectable } from "@angular/core"
-import { Storage } from "@ionic/storage-angular"
-import { StorageService } from "src/app/common/services/storage.service"
-import { GameConfigEvents } from "./game-config.events"
 import { GameDifficultyStorage } from "./game-difficulty.storage"
-import { GameLengthConfig } from "./game-length.config"
+import { GameLengthStorage } from "./game-length.config"
 import { GameDifficulty, GameLength } from "./settings.interface"
 
 export interface GameConfigObject {
@@ -15,7 +12,7 @@ export interface GameConfigObject {
   providedIn: "root",
 })
 export class GameConfig {
-  constructor(public difficulty: GameDifficultyStorage, public length: GameLengthConfig) {}
+  constructor(public difficulty: GameDifficultyStorage, public length: GameLengthStorage) {}
 
   async getConfig(): Promise<GameConfigObject> {
     const difficulty = await this.difficulty.get()
