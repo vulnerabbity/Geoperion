cc621faf267a65ed6cda850fa96c7be5c78b973c
vulnerabbity <vulnerabbity@example.com>
2022-05-29 22:15:40 +0700

    refactoring: (renaming) (settings module):
    Renamed GameLengthConfig =>  GameLengthStorage

--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from "@angular/core"
-import { Storage as IonStorage } from "@ionic/storage-angular"
 import { GameConfig } from "./game-config"
 import { GameConfigState } from "./game-config.state"
 import { GameDifficultyStorage } from "./game-difficulty.storage"
-import { GameLengthConfig } from "./game-length.config"
+import { GameLengthStorage } from "./game-length.config"
 import { AppThemeSettingsModule } from "./theme/theme-settings.module"
 
 @NgModule({
   imports: [AppThemeSettingsModule],
-  providers: [GameConfig, GameDifficultyStorage, GameLengthConfig],
+  providers: [GameConfig, GameDifficultyStorage, GameLengthStorage],
 })
 export class GameSettingsModule {
   // init on load
-  constructor(private storage: IonStorage, private configState: GameConfigState) {
-    storage.create()
+  constructor(private configState: GameConfigState) {
     configState.init()
   }
 }
