fc63ffeaab1761ccd69f3cfb095e1534a00d417e
vulnerabbity <vulnerabbity@example.com>
2022-06-22 18:26:21 +0700

    refactoring: (renaming) changed GameConfig to GameStorage

--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core"
-import { GameConfig } from "./game-config"
+import { GameStorage } from "./game-config"
 import { GameConfigState } from "./game-config.state"
 import { GameDifficultyStorage } from "./game-difficulty.storage"
 import { GameLengthStorage } from "./game-length.config"
@@ -7,7 +7,7 @@ import { AppThemeSettingsModule } from "./theme/theme-settings.module"
 
 @NgModule({
   imports: [AppThemeSettingsModule],
-  providers: [GameConfig, GameDifficultyStorage, GameLengthStorage],
+  providers: [GameStorage, GameDifficultyStorage, GameLengthStorage],
 })
 export class GameSettingsModule {
   // init on load
