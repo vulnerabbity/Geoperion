0ff9b6575f1b12d76509bab1960fabc3b24128f8
vulnerabbity <vulnerabbity@example.com>
2022-06-22 18:26:48 +0700

    refactoring: removed unused code

--- src/app/features/settings/theme/theme-settings.module.ts
+++ src/app/features/settings/theme/theme-settings.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from "@angular/core"
 import { AppThemeBackgroundStorage } from "./theme-background.storage"
-import { AppThemeStorage } from "./theme.storage"
-import { AppThemeEventsBus } from "./theme.events-bus"
-import { AppThemeEventsHandler } from "./theme.events-handler"
 import { AppThemeState } from "./theme.state"
 
 @NgModule({
-  providers: [AppThemeBackgroundStorage, AppThemeState, AppThemeEventsBus, AppThemeState],
+  providers: [AppThemeBackgroundStorage, AppThemeState, AppThemeState],
 })
-export class AppThemeSettingsModule {
-  // init on module load
-  constructor(eventsHandler: AppThemeEventsHandler) {}
-}
+export class AppThemeSettingsModule {}
