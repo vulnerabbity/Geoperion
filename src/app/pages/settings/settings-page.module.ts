b49796f9b72c27a0c7693e45103489766a935890
vulnerabbity <vulnerabbity@example.com>
2022-06-22 18:50:36 +0700

    add events handler initialization

--- src/app/pages/settings/settings-page.module.ts
+++ src/app/pages/settings/settings-page.module.ts
@@ -3,6 +3,7 @@ import { ColorPickerModule } from "ngx-color-picker"
 import { AppCommonModule } from "src/app/common/common.module"
 import { SettingsPageDifficultyComponent } from "./difficulty/difficulty.component"
 import { SettingsPageGameLengthComponent } from "./game-length/game-length.component"
+import { SettingsPageEventsHandler } from "./settings.events-handler"
 import { SettingsPage } from "./settings.page"
 import { SettingsPageAccentComponent } from "./theme/accent/accent.component"
 import { SettingsPageBackgroundComponent } from "./theme/background/background.component"
@@ -17,4 +18,7 @@ import { SettingsPageBackgroundComponent } from "./theme/background/background.c
     SettingsPageAccentComponent,
   ],
 })
-export class SettingsPageModule {}
+export class SettingsPageModule {
+  // init on module load
+  constructor(private eventsHandler: SettingsPageEventsHandler) {}
+}
