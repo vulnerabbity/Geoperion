bc060adf5a3fe634b7fb219f8db2305e72754ec7
vulnerabbity <vulnerabbity@example.com>
2022-06-22 18:30:14 +0700

    add storage usage to AppThemeState

--- src/app/features/settings/theme/theme.state.ts
+++ src/app/features/settings/theme/theme.state.ts
@@ -1,14 +1,38 @@
+import { Injectable } from "@angular/core"
 import { ReplaySubject } from "rxjs"
 import { AppTheme } from "../settings.interface"
-import { getDefaultThemeHexAccent } from "./theme-accent.storage"
-import { getDefaultBackgroundTheme } from "./theme-background.storage"
+import { AppThemeAccentStorage, getDefaultThemeHexAccent } from "./theme-accent.storage"
+import { AppThemeBackgroundStorage, getDefaultBackgroundTheme } from "./theme-background.storage"
 
+@Injectable({
+  providedIn: "root",
+})
 export class AppThemeState {
-  theme$ = new ReplaySubject<AppTheme>()
+  currentTheme$ = new ReplaySubject<AppTheme>()
+
+  constructor(
+    private readonly backgroundStorage: AppThemeBackgroundStorage,
+    private readonly accentStorage: AppThemeAccentStorage,
+  ) {
+    this.init()
+  }
 
   getDefault(): AppTheme {
     const background = getDefaultBackgroundTheme()
     const hexAccent = getDefaultThemeHexAccent()
+
+    return { background, hexAccent }
+  }
+
+  private async init() {
+    const theme = await this.getTheme()
+
+    this.currentTheme$.next(theme)
+  }
+
+  private async getTheme(): Promise<AppTheme> {
+    const background = await this.backgroundStorage.get()
+    const hexAccent = await this.accentStorage.get()
 
     return { background, hexAccent }
   }
