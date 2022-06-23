ece36e1c821359c8b74c77ab7794267442f710f4
vulnerabbity <vulnerabbity@example.com>
2022-06-24 00:09:01 +0700

    translated accent component

--- src/app/pages/settings/theme/accent/accent.component.ts
+++ src/app/pages/settings/theme/accent/accent.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core"
+import { LanguageServiceInstance } from "src/app/common/language/language.service"
 import { getDefaultThemeHexAccent } from "src/app/features/settings/theme/theme-accent.storage"
 import { SettingsPageEvents } from "../../settings.events"
 
@@ -10,6 +11,8 @@ import { SettingsPageEvents } from "../../settings.events"
 export class SettingsPageAccentComponent {
   @Input("hexAccent")
   hexAccent: string = getDefaultThemeHexAccent()
+
+  translation = LanguageServiceInstance.translation.settingsPage.accent
 
   constructor(private events: SettingsPageEvents) {}
 
