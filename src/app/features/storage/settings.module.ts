0b9e8f21406d090d94f97e92c8ce9e31d473c7bd
vulnerabbity <vulnerabbity@example.com>
2022-06-24 12:14:40 +0700

    refactoring: simplifyed settings module

--- src/app/features/storage/settings.module.ts
+++ src/app/features/storage/settings.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from "@angular/core"
 import { GameStorage } from "./game-storage"
-import { GameConfigState } from "./game-config.state"
-import { AppThemeSettingsModule } from "./theme/theme-settings.module"
 import { GameDifficultyStorage } from "./general/game-difficulty.storage"
 import { GameLengthStorage } from "./general/game-length.storage"
 
 @NgModule({
-  imports: [AppThemeSettingsModule],
   providers: [GameStorage, GameDifficultyStorage, GameLengthStorage],
 })
-export class GameSettingsModule {
-  // init on load
-  constructor(private configState: GameConfigState) {
-    configState.init()
-  }
-}
+export class GameSettingsModule {}
