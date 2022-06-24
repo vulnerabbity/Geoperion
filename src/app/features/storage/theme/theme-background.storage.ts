1cd14fcedac6604f7ad5b8ca59063997baf02096
vulnerabbity <vulnerabbity@example.com>
2022-06-24 12:16:54 +0700

    reafactoring: (Background storage) moved interface inside storage file

--- src/app/features/storage/theme/theme-background.storage.ts
+++ src/app/features/storage/theme/theme-background.storage.ts
@@ -1,16 +1,26 @@
 import { Injectable } from "@angular/core"
 import { StorageService } from "src/app/common/services/storage.service"
-import { AppBackgroundTheme } from "../settings.interface"
+
+export enum BackgroundThemes {
+  Light = "light",
+  Dark = "dark",
+  Black = "black",
+  Nord = "nord",
+  Github = "github",
+  Retro = "retro",
+}
+
+export type BackgroundTheme = `${BackgroundThemes}`
 
 @Injectable({
   providedIn: "root",
 })
-export class AppThemeBackgroundStorage extends StorageService<AppBackgroundTheme> {
+export class ThemeBackgroundStorage extends StorageService<BackgroundTheme> {
   readonly storageKey = "theme-background"
 
   readonly defaultValue = getDefaultBackgroundTheme()
 }
 
-export function getDefaultBackgroundTheme(): AppBackgroundTheme {
+export function getDefaultBackgroundTheme(): BackgroundTheme {
   return "light"
 }
