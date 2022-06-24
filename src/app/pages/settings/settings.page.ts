b07ef92bd36d8565fa70a1ec16a0011a29e92984
vulnerabbity <vulnerabbity@example.com>
2022-06-24 14:05:24 +0700

    (settings page) add current values for settings

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core"
+import { Component, OnDestroy } from "@angular/core"
 import { LanguageServiceInstance } from "src/app/common/language/language.service"
+import { GameStorage } from "src/app/features/storage/game-storage"
 
 @Component({
   templateUrl: "./settings.page.html",
   styleUrls: ["./settings.page.scss"],
 })
-export class SettingsPage {
+export class SettingsPage implements OnDestroy {
   translation = LanguageServiceInstance.translation.settingsPage
+
+  configSnapshot = this.gameStorage.getConfigReference()
+
+  private configSub = this.updateConfigOnChange()
+
+  constructor(private gameStorage: GameStorage) {}
+
+  ngOnDestroy(): void {
+    this.configSub.unsubscribe()
+  }
+
+  updateConfigOnChange() {
+    return this.gameStorage.configSnapshot$.subscribe(newConfig => {
+      this.configSnapshot = newConfig
+    })
+  }
 }
