f998c4c224469337956d3d29305ad3c0f4c5bd7f
vulnerabbity <vulnerabbity@example.com>
2022-06-24 12:17:07 +0700

    refactoring: deleted unused code

--- src/app/features/storage/theme/theme-settings.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from "@angular/core"
-import { AppThemeBackgroundStorage } from "./theme-background.storage"
-import { AppThemeState } from "./theme.state"
-
-@NgModule({
-  providers: [AppThemeBackgroundStorage, AppThemeState, AppThemeState],
-})
-export class AppThemeSettingsModule {}
