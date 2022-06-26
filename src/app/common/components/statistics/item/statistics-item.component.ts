b8f132f0319aaf7490d35a554bf0ba75bcbfa74d
vulnerabbity <vulnerabbity@example.com>
2022-06-26 23:32:30 +0700

    implemented full working statisics item

--- src/app/common/components/statistics/item/statistics-item.component.ts
+++ src/app/common/components/statistics/item/statistics-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from "@angular/core"
+import { LanguageServiceInstance } from "src/app/common/language/language.service"
+import { DetailedGameStatisticsGenerator } from "src/app/features/statistics/detailed-statistics"
 import { GameStatistics } from "src/app/features/statistics/statistics"
-import { GameStatisticsGenerator } from "src/app/features/statistics/statistics-generator"
+import { GameStatisticsManager } from "src/app/features/statistics/statistics-manager"
 
 @Component({
   selector: "statistics-item",
@@ -10,4 +12,23 @@ import { GameStatisticsGenerator } from "src/app/features/statistics/statistics-
 export class StatisticsItemComponent {
   @Input()
   statistic = GameStatistics.getDefault()
+
+  get translation() {
+    return LanguageServiceInstance.translation.statistics
+  }
+
+  private get detailedStatistics() {
+    return DetailedGameStatisticsGenerator.generate(this.statistic)
+  }
+
+  getTimePassed() {
+    const secondsPassed = this.detailedStatistics.secondsPassed ?? 0
+    const millisecondsPassed = secondsPassed * 1000
+
+    return new Date(millisecondsPassed)
+  }
+
+  async delete() {
+    await GameStatisticsManager.removeById(this.statistic.id)
+  }
 }
